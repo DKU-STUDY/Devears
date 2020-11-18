37ce28e7147a3f102dd197584bc591e0473404ff
junilHwang <junil.h@kakao.com>
2020-11-18 19:10:26 +0900

    feat: GithubAdapter 추가 완료

---
+++ devears-api/src/main/java/org/dkustudy/devears/api/adapter/github/GithubAdapter.java
@@ -0,0 +1,12 @@
+package org.dkustudy.devears.api.adapter.github;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GithubAdapter {
+
+    public String getAuth() {
+        return "test";
+    }
+
+}
---
+++ devears-api/src/main/java/org/dkustudy/devears/api/adapter/github/GithubPath.java
@@ -0,0 +1,7 @@
+package org.dkustudy.devears.api.adapter.github;
+
+public class GithubPath {
+    private GithubPath() {}
+
+    public static final String DOMAIN_URL = "https://github.com";
+}
