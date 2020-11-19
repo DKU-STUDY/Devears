ebafdcdf228cc547617579ed313275c9e1e9f3d3
junilHwang <junil.h@kakao.com>
2020-11-20 00:32:13 +0900

    fix: testCode 제거

--- devears-api/src/test/java/org/dkustudy/devears/api/adapter/github/GithubAdapterTest.java
+++ devears-api/src/test/java/org/dkustudy/devears/api/adapter/github/GithubAdapterTest.java
@@ -7,15 +7,4 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GithubAdapterTest {
-
-    private final GithubAdapter githubAdapter = new GithubAdapter(
-        new RestClientConfig().githubClient()
-    );
-
-    @Test
-    public void 인증_테스트() {
-        assertThat(githubAdapter.getAuth().get("current_user_url"))
-            .isEqualTo("https://api.github.com/user");
-    }
-
 }
