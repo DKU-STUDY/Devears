4c4848db07facfe39233ae1699d19ce2e6af72fd
junilHwang <junil.h@kakao.com>
2020-11-20 01:13:26 +0900

    refactor: GithubFacade를 이용하여 리팩토링

--- devears-api/src/main/java/org/dkustudy/devears/api/endpoint/github/controller/GithubController.java
+++ devears-api/src/main/java/org/dkustudy/devears/api/endpoint/github/controller/GithubController.java
@@ -1,16 +1,11 @@
 package org.dkustudy.devears.api.endpoint.github.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.dkustudy.devears.api.adapter.github.GithubAdapter;
-import org.dkustudy.devears.api.adapter.github.request.GithubAccessTokenRequest;
-import org.dkustudy.devears.api.adapter.github.response.GithubAccessTokenResponse;
 import org.dkustudy.devears.api.endpoint.constant.ApiPath;
-import org.dkustudy.devears.api.property.GithubProperty;
+import org.dkustudy.devears.api.endpoint.facade.GithubFacade;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -19,30 +14,17 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class GithubController {
 
-    private final GithubAdapter githubAdapter;
-    private final GithubProperty githubProperty;
+    private final GithubFacade githubFacade;
 
     @GetMapping(ApiPath.Github.LOGIN)
-    public void getAuth(HttpServletResponse response) throws IOException {
-        response.sendRedirect(
-            UriComponentsBuilder
-                .fromHttpUrl("https://github.com/login/oauth/authorize")
-                .queryParam("scope", githubProperty.getScope())
-                .queryParam("client_id", githubProperty.getClientId())
-                .build()
-                .toString()
-        );
+    public void login(HttpServletResponse response) throws IOException {
+        response.sendRedirect(githubFacade.getLoginURL());
     }
 
     @GetMapping(ApiPath.Github.LOGIN_CALLBACK)
-    public GithubAccessTokenResponse initAuth(@RequestParam String code) {
-        return githubAdapter.getAccessToken(
-            GithubAccessTokenRequest.builder()
-                .code(code)
-                .client_id(githubProperty.getClientId())
-                .client_secret(githubProperty.getClientSecret())
-                .build()
-        );
+    public void initAuth(@RequestParam String code, HttpServletResponse response) throws IOException {
+        githubFacade.authorization(code);
+        response.sendRedirect("/");
     }
 
 }
