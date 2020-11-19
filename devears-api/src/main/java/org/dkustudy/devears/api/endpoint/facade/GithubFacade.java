53f875d12681c445ab940d6b1a362ad7a135150e
junilHwang <junil.h@kakao.com>
2020-11-20 02:48:57 +0900

    feat: Spring Security 제거

--- devears-api/src/main/java/org/dkustudy/devears/api/endpoint/facade/GithubFacade.java
+++ devears-api/src/main/java/org/dkustudy/devears/api/endpoint/facade/GithubFacade.java
@@ -1,7 +1,6 @@
 package org.dkustudy.devears.api.endpoint.facade;
 
 import lombok.RequiredArgsConstructor;
-import org.dkustudy.devears.api.config.security.JwtTokenProvider;
 import org.dkustudy.devears.api.endpoint.service.GithubAuthService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -13,16 +12,19 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 @RequiredArgsConstructor
 public class GithubFacade {
+    private static final int ACCESS_TOKEN_EXPIRATION = 60 * 60 * 24;
+
     private final GithubAuthService githubAuthService;
-    private final JwtTokenProvider jwtTokenProvider;
 
     public String getLoginURL() {
         return githubAuthService.getLoginURL();
     }
 
     public void authorization(String code) {
-        String accessToken = githubAuthService.getAccessToken(code);
-        jwtTokenProvider.createToken(accessToken);
+        Cookie cookie = new Cookie("accessToken", githubAuthService.getAccessToken(code));
+        cookie.setMaxAge(ACCESS_TOKEN_EXPIRATION);
+        cookie.setPath("/");
+        addCookie(cookie);
     }
 
     private static void addCookie(Cookie cookie) {
