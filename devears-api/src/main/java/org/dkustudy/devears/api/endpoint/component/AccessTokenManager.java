104124fb5cce4e0ba6661d708386971b5f46997a
junilHwang <junil.h@kakao.com>
2020-11-21 23:58:48 +0900

    feat: accessToken을 JWT로 만들기

--- devears-api/src/main/java/org/dkustudy/devears/api/endpoint/component/AccessTokenManager.java
+++ devears-api/src/main/java/org/dkustudy/devears/api/endpoint/component/AccessTokenManager.java
@@ -1,22 +1,52 @@
 package org.dkustudy.devears.api.endpoint.component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.dkustudy.devears.api.property.JwtProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Base64;
+import java.util.Date;
 
 @Component
+@RequiredArgsConstructor
 public class AccessTokenManager {
     public static final int EXPIRATION = 60 * 60 * 24;
-    public static final String KEY = "accessToken";
+    public static final String KEY = "authToken";
 
-    public void setToken(String token) {
-        Cookie cookie = new Cookie(KEY, token);
+    private final JwtProperty jwtProperty;
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        secretKey = Base64.getEncoder()
+                          .encodeToString(jwtProperty.getSecret().getBytes());
+    }
+
+    public void createAuth(String accessToken) {
+        String jwt = getJwtByAccessToken(accessToken);
+        Cookie cookie = new Cookie(KEY, jwt);
         cookie.setMaxAge(EXPIRATION);
         cookie.setPath("/");
         addCookie(cookie);
+    }
+
+    private String getJwtByAccessToken(String accessToken) {
+        Claims claims = Jwts.claims().setSubject(accessToken); // claim 생성
+        Date now = new Date();
+        return Jwts.builder()
+            .setClaims(claims) // claim 지정
+            .setIssuedAt(now) // 토큰 발행 일자 지정
+            .setExpiration(new Date(now.getTime() + EXPIRATION * 1000)) // 유효 시간 지정
+            .signWith(SignatureAlgorithm.HS256, secretKey) // 암호화 알고리즘, secret 값 지정
+            .compact(); // 위의 내용을 압축 후 반환
     }
 
     public void removeToken() {
@@ -24,6 +54,28 @@ public class AccessTokenManager {
         cookie.setMaxAge(0);
         cookie.setPath("/");
         addCookie(cookie);
+    }
+
+    public String getAccessTokenByJwt(String jwt) {
+        return getClaimsByJwt(jwt)
+                   .getSubject();
+    }
+
+    public boolean validateJwt(String jwt) {
+        try {
+            return !getClaimsByJwt(jwt)
+                    .getExpiration()
+                    .before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims getClaimsByJwt(String jwt) {
+        return Jwts.parser()
+                   .setSigningKey(secretKey)
+                   .parseClaimsJws(jwt)
+                   .getBody();
     }
 
     private static void addCookie(Cookie cookie) {
