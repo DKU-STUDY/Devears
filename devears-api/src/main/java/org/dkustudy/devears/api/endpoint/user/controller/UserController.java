eacfb31c9c5b8c165883d7399fa490e3d61e7b96
junilHwang <junil.h@kakao.com>
2020-11-21 23:59:19 +0900

    refactor: ResponseStatus로 변경

--- devears-api/src/main/java/org/dkustudy/devears/api/endpoint/user/controller/UserController.java
+++ devears-api/src/main/java/org/dkustudy/devears/api/endpoint/user/controller/UserController.java
@@ -1,16 +1,15 @@
 package org.dkustudy.devears.api.endpoint.user.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.http.HttpStatus;
 import org.dkustudy.devears.api.endpoint.component.AccessTokenManager;
 import org.dkustudy.devears.api.endpoint.constant.Path;
 import org.dkustudy.devears.api.endpoint.facade.UserFacade;
 import org.dkustudy.devears.api.endpoint.user.data.response.UserResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,14 +19,16 @@ public class UserController {
 
     @GetMapping(Path.UserApi.USER)
     public UserResponse getUser(
-        @CookieValue(name = AccessTokenManager.KEY, defaultValue = "") String accessToken
+        @CookieValue(name = AccessTokenManager.KEY, defaultValue = "") String jwt
     ) {
-        return userFacade.getUserByToken(accessToken);
+        return userFacade.getUserByToken(jwt);
     }
 
     @GetMapping(Path.UserApi.LOGOUT)
-    public void logout(HttpServletResponse response) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public boolean logout() {
         userFacade.logout();
-        response.setStatus(HttpStatus.SC_NO_CONTENT);
+        return true;
     }
+
 }
