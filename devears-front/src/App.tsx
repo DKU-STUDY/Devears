7ee947ebdc291c46244b2e28a3c996749dff9d66
junilHwang <junil.h@kakao.com>
2020-12-17 23:46:10 +0900

    docs: React Router에 대한 tip 추가 완료

--- devears-front/src/App.tsx
+++ devears-front/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import Main from "./views/Main";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route>
-
-        </Route>
+        <DefaultLayout exact path="/" component={Main} />
       </Switch>
     </BrowserRouter>
   );
--- docs/tips/front/README.md
+++ docs/tips/front/README.md
@@ -3,3 +3,10 @@
 - [CRA로 만든 리액트 프로젝트에서 eject하지 않고 optional chaining 사용하기](https://sustainable-dev.tistory.com/126)
   - customize-cra
   - react-app-rewired
+
+## React Router Layout
+
+- [React Router](https://reactrouter.com/web/guides/quick-start)
+- [Reusing layouts in React Router 4](https://simonsmith.io/reusing-layouts-in-react-router-4)
+- [React Router v4 with multiple layouts](https://stackoverflow.com/a/46201798)
+
