5fccf88466899670720ba97273e1d524072872bf
junilHwang <junil.h@kakao.com>
2020-12-18 22:59:35 +0900

    feat: Header style 작업 중

--- devears-front/src/layouts/default/Header.tsx
+++ devears-front/src/layouts/default/Header.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import logo from "assets/image/logo.svg";
+import css from "styled-jsx/css";
 
 const Header: React.FC = () => {
   return (
     <>
       <header id="site-header">
-        <Link to="/"><img src={logo} alt="devears" width="100" /></Link>
+        <Link to="/"><img src={logo} alt="devears" width="100"/></Link>
       </header>
-      <style jsx></style>
+      <style jsx>{headerStyles}</style>
     </>
   )
 }
+
+const headerStyles = css`
+  header { border-bottom: 1px solid #ddd; background: #f5f5f5; }
+`;
 
 export default React.memo(Header);
