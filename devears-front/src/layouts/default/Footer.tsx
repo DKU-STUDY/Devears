ef1670629f28b70c62744f77c4afb4b9be75eddb
junilHwang <junil.h@kakao.com>
2020-12-21 22:32:16 +0900

    chore: ant-design 패키지 추가

--- devears-front/src/layouts/default/Footer.tsx
+++ devears-front/src/layouts/default/Footer.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import css from "styled-jsx/css";
+import Logo from "assets/image/logo.svg";
 
 const Footer: React.FC = () => {
   return (
     <>
       <footer>
         <div className="container">
-          <div className="">
+          <div className="logo">
+            <img src={Logo} alt="devears" height="50" />
           </div>
           <div>
             <p className="copyright">
               Copyright &copy; 2021 Devears All Right Reserved.
             </p>
+            <ul className="info">
+              <li></li>
+            </ul>
           </div>
         </div>
       </footer>
@@ -27,9 +32,20 @@ const footerStyles = css`
     padding: 20px 0;
   }
   
+  .container {
+    display: flex;
+    align-items: center;
+  }
+  
   .copyright {
     color: #bbc0c4;
     text-transform: uppercase;
+  }
+  
+  .logo {
+    img {
+      filter: grayscale(100%);
+    }
   }
 `;
 
