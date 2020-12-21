a75daab9e635b70983ec24d18444245a84de1b51
junilHwang <junil.h@kakao.com>
2020-12-21 23:31:48 +0900

    feat: Footer에 메뉴 및 사이트 정보 추가 완료

--- devears-front/src/layouts/default/Footer.tsx
+++ devears-front/src/layouts/default/Footer.tsx
@@ -1,22 +1,34 @@
 import React from "react";
 import css from "styled-jsx/css";
 import Logo from "assets/image/logo.svg";
+import {MailOutlined, PhoneFilled} from "@ant-design/icons";
 
 const Footer: React.FC = () => {
   return (
     <>
       <footer>
         <div className="container">
+          <div>
+            <ul className="menu">
+              <li><a href="#!">이용약관</a></li>
+              <li><a href="#!">개인정보 처리방침</a></li>
+            </ul>
+            <ul className="info">
+              <li>
+                <PhoneFilled />
+                <span>+82-10-5764-4483</span>
+              </li>
+              <li>
+                <MailOutlined />
+                <span>junil.h@kakao.com</span>
+              </li>
+            </ul>
+            <p className="copyright">
+              Copyright &copy; 2021 <strong>Devears</strong> All Right Reserved.
+            </p>
+          </div>
           <div className="logo">
             <img src={Logo} alt="devears" height="50" />
-          </div>
-          <div>
-            <p className="copyright">
-              Copyright &copy; 2021 Devears All Right Reserved.
-            </p>
-            <ul className="info">
-              <li></li>
-            </ul>
           </div>
         </div>
       </footer>
@@ -35,11 +47,21 @@ const footerStyles = css`
   .container {
     display: flex;
     align-items: center;
+    justify-content: space-between;
+    font-size: 13px;
   }
   
   .copyright {
     color: #bbc0c4;
     text-transform: uppercase;
+  }
+  
+  .menu {
+    display: flex;
+  }
+  
+  .info {
+    display: flex;
   }
   
   .logo {
