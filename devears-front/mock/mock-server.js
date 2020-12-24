1eca7bd0b9803afa9a33b2e90cc661a7ff3f6908
junilhwang <tjsdlf4261@naver.com>
2020-12-24 17:28:41 +0900

    feat: [mock] crew api 작성 완료

--- devears-front/mock/mock-server.js
+++ devears-front/mock/mock-server.js
@@ -3,10 +3,23 @@ const mockServer = jsonServer.create();
 const { join } = require("path");
 const router = jsonServer.router(join(__dirname, 'data.json'));
 const middlewares = jsonServer.defaults();
+const { normalize } = require('./utils');
 
 mockServer.use(jsonServer.bodyParser);
 mockServer.use(middlewares);
+
+mockServer.get("/api/crew", (req, res) => {
+  const data = normalize(require("./data.json"));
+  console.log(data);
+  const { crew: { ids, entries }, users } = data;
+  res.json(ids.map(id => ({
+    ...entries[id],
+    writer: users.entries[entries[id].writer]
+  })));
+});
+
 mockServer.use("/api", router);
+
 mockServer.listen(8080, () => {
   console.log('JSON Server is running')
 });
