a85df92396f39df184ed471cfa0ea1f78e93684c
JunilHwang <junil.h@kakao.com>
2020-12-25 18:50:49 +0900

    fix: [front] Crew sagas 오류 수정

--- devears-front/src/data/crew/sagas.ts
+++ devears-front/src/data/crew/sagas.ts
@@ -2,18 +2,19 @@ import { call, put, takeEvery } from "redux-saga/effects";
 import { CrewService } from "services";
 import * as actions from "./actions";
 import * as actionTypes from "./actionTypes";
+import {CrewResponse} from "../../domain";
 
 function* getCrews() {
   try {
     yield put(actions.getCrewsLoading());
-    yield call(CrewService.getAllCrew);
-    yield put(actions.getCrewsSuccess());
+    const data: CrewResponse[] = yield call(CrewService.getAllCrew);
+    yield put(actions.getCrewsSuccess(data));
   } catch (e) {
     console.error(e);
     yield put(actions.getCrewsFailure());
   }
 }
 
-export default function* crewSagaRegister() {
+export default function* crewSagas() {
   yield takeEvery(actionTypes.GET_CREWS, getCrews);
 }
