7729c2d41f7fec90a6d6fa354a05f05db417388c
JunilHwang <junil.h@kakao.com>
2020-12-25 18:48:36 +0900

    feat: [front] Crew reducer 작성 중

--- devears-front/src/data/crew/reducers.ts
+++ devears-front/src/data/crew/reducers.ts
@@ -1,5 +1,46 @@
-export default function crewReducer(state = {}, action = {}) {
-  switch (action.type) {
+import {CrewAction} from "./actions";
+import * as actionTypes from "./actionTypes";
 
+const initState = {
+  [actionTypes.GET_CREWS]: {
+    asked: false,
+    loading: false,
+    failure: false,
+    success: []
+  },
+}
+
+export default function crewReducer(state = initState, action: CrewAction) {
+  switch (action.type) {
+    case actionTypes.GET_CREWS_LOADING:
+      return {
+        ...state,
+        [actionTypes.GET_CREWS]: {
+          ...state[actionTypes.GET_CREWS],
+          asked: true,
+          loading: true,
+          failure: false,
+        }
+      }
+    case actionTypes.GET_CREWS_FAILURE:
+      return {
+        ...state,
+        [actionTypes.GET_CREWS]: {
+          ...state[actionTypes.GET_CREWS],
+          asked: false,
+          loading: false,
+          failure: true,
+        }
+      }
+    case actionTypes.GET_CREWS_SUCCESS:
+      return {
+        ...state,
+        [actionTypes.GET_CREWS]: {
+          asked: false,
+          loading: false,
+          failure: false,
+          success: action.payload
+        }
+      }
   }
 }
