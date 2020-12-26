d8790cf9c405fb1e49e7bd947a0976924015db33
JunilHwang <junil.h@kakao.com>
2020-12-26 17:28:45 +0900

    feat: [front] rootReducer에 crewReducer 등록 완료

--- devears-front/src/data/rootReducer.ts
+++ devears-front/src/data/rootReducer.ts
@@ -1,11 +1,13 @@
 import { connectRouter } from 'connected-react-router';
 import { combineReducers } from 'redux';
 import { History } from 'history';
+import crew from "./crew/reducers";
 
 const createRootReducer = (history: History) => combineReducers({
   router: connectRouter(history),
+  crew,
 });
 
 export {
-  createRootReducer
+  createRootReducer,
 }
