f189e603f2b7c534a2faf29ac15be49759265164
JunilHwang <junil.h@kakao.com>
2020-12-26 17:24:56 +0900

    feat: [front] getCrews selector 작성 완료

--- devears-front/src/data/crew/selectors.ts
+++ devears-front/src/data/crew/selectors.ts
@@ -1,1 +1,6 @@
-export default {};
+import * as actionTypes from "./actionTypes";
+import {CrewState} from "./reducers";
+import {CrewResponse} from "../../domain";
+
+export const getCrews = (state: CrewState): CrewResponse[] =>
+  state[actionTypes.GET_CREWS].success;
