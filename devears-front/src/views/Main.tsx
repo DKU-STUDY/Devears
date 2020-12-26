8fe859916d6fed97cf134bd46fac52deaf16001c
JunilHwang <junil.h@kakao.com>
2020-12-26 17:46:52 +0900

    feat: [front] Main에서 getCrews dispatch 완료

--- devears-front/src/views/Main.tsx
+++ devears-front/src/views/Main.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Typography} from "antd";
 import { CrewService } from "../services";
+import {useDispatch, useSelector} from "react-redux";
+import selectors from "../data/rootSelectors";
+import actions from "../data/rootActions";
 
 CrewService.getAllCrew().then(console.log);
 
 const { Title } = Typography;
 
 const Main: React.FC = () => {
+  const dispatch = useDispatch();
+  const crews = useSelector(selectors.crew.getCrews);
+
+  useEffect(() => {
+    dispatch(actions.crew.getCrews());
+  }, [dispatch]);
+
   return (
     <main>
       <div className="container">
         <section>
           <Title level={2}>스터디 크루</Title>
+          { crews.length }
         </section>
         <section>
           <Title level={2}>참여 중인 스터디</Title>
