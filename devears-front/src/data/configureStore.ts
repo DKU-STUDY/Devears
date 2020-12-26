66fee661b990534b47b3f76acee363b91ccb6ef8
JunilHwang <junil.h@kakao.com>
2020-12-26 17:46:05 +0900

    refactor [front] configureStore에서 logger 제거

--- devears-front/src/data/configureStore.ts
+++ devears-front/src/data/configureStore.ts
@@ -9,22 +9,12 @@ const history = createBrowserHistory();
 const sagaMiddleware = createSagaMiddleware();
 const rootReducer = createRootReducer(history);
 
-const logger = (store: any) => (next: any) => (action: any) => {
-  console.group(action.type);
-  console.info('dispatching', action);
-  const result = next(action);
-  console.log('next state', store.getState());
-  console.groupEnd();
-  return result;
-}
-
 export default function configureStore() {
   const store = createStore(
     rootReducer,
     applyMiddleware(
       sagaMiddleware,
       routerMiddleware(history),
-      logger
     ),
   );
 
