6bd33245bb9f8d142a2f83bb50946463566f3ecf
JunilHwang <junil.h@kakao.com>
2020-12-27 22:23:15 +0900

    feat: [front] User Profile Style 작업 완료

--- devears-front/src/components/card/CrewCard.tsx
+++ devears-front/src/components/card/CrewCard.tsx
@@ -4,36 +4,58 @@ import UserProfile from "./UserProfile";
 import {CrewResponse} from "../../domain";
 import Conditional from "../../hocs/Conditional";
 import NoneImage from "./NoneImage";
+import { Link } from "react-router-dom";
 
-const CrewCard: React.FC<CrewResponse> = ({ thumbnailUrl, name, description, writer }: CrewResponse) => {
+const CrewCard: React.FC<CrewResponse> = ({ idx, thumbnailUrl, name, description, writer }: CrewResponse) => {
   return (
     <>
-      <article>
-        <Conditional condition={thumbnailUrl.length > 0}>
-          <figure className="image">
-            <img src={thumbnailUrl} alt={name} />
-          </figure>
-        </Conditional>
-        <Conditional condition={thumbnailUrl.length === 0}>
-          <NoneImage />
-        </Conditional>
-        <h4>{name}</h4>
-        <p>{description}</p>
-        <UserProfile
-          id={writer.id}
-          profileImageUrl={writer.profileImageUrl} />
-      </article>
+      <Link to={`/crew/${idx}`} className={crewWrap.className}>
+        <article>
+          <Conditional condition={thumbnailUrl.length > 0}>
+            <figure>
+              <img src={thumbnailUrl} alt={name} />
+            </figure>
+          </Conditional>
+          <Conditional condition={thumbnailUrl.length === 0}>
+            <NoneImage />
+          </Conditional>
+          <h4>{name}</h4>
+          <p>{description}</p>
+          <UserProfile
+            id={writer.id}
+            profileImageUrl={writer.profileImageUrl} />
+        </article>
+      </Link>
+      {crewWrap.styles}
       <style jsx>{crewCardStyles}</style>
     </>
   );
 }
 
-const crewCardStyles = css`
-  article {
+const crewWrap = css.resolve`
+  a {
+    display: block;
     width: calc(20% - 10px);
     margin: 0 5px;
   }
-  
+`;
+const crewCardStyles = css`
+
+  figure {
+    margin: 0;
+    padding: 60% 0 0;
+    position: relative;
+    
+    img {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+
   h4 {
     font-size: 17px;
     padding: 0;
@@ -46,7 +68,6 @@ const crewCardStyles = css`
   p {
     font-size: 14px;
     line-height: 130%;
-    height: calc(14px * 1.3 * 3);
     text-overflow: ellipsis;
     overflow: hidden;
     display: -webkit-box;
--- devears-front/src/components/card/UserProfile.tsx
+++ devears-front/src/components/card/UserProfile.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import css from "styled-jsx/css";
 
 export interface UserProfileProps {
   id: string;
@@ -10,13 +11,44 @@ const UserProfile: React.FC<UserProfileProps> = ({ id, profileImageUrl }) => {
   const userPageLink = `/@${id}`;
 
   return (
-    <Link to={userPageLink}>
+    <Link to={userPageLink} className={wrapResolve.className}>
       <figure>
         <img src={profileImageUrl} alt={id} />
       </figure>
       <span>{id}</span>
+      {wrapResolve.styles}
+      <style jsx>{userProfileStyles}</style>
     </Link>
   )
 }
+
+const wrapResolve = css.resolve`
+  a {
+    margin-top: 10px;
+    display: flex;
+  }
+`;
+
+const userProfileStyles = css`
+  figure {
+    border-radius: 20px;
+    width: 24px;
+    height: 24px;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+    
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  
+  span {
+    font-size: 14px;
+    margin-left: 5px;
+    color: #666;
+  }
+`;
 
 export default React.memo(UserProfile);
